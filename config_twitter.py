04ed4cbd0e60b15443797e7b7f4a40c9a7a7401a
jasturiano <48779823+jasturiano@users.noreply.github.com>
2019-03-25 11:33:00 -0600

    Create config_twitter.py

---
+++ config_twitter.py
@@ -0,0 +1,7 @@
+#These information comes from Twitter API
+#Create a Twitter Account and Get These Information from apps.twitter.com
+
+consumer_key = 'xxxx'
+consumer_secret = 'xxxx'
+access_token = 'xxxx'
+access_secret = 'xxxxx'
