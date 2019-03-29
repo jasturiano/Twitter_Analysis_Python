a4b9c9d945219492160f8abac0cdf9cfa1edd9d3
J. Asturiano <48779823+jasturiano@users.noreply.github.com>
2019-03-29 15:35:39 -0600

    Update spark_stream.py

--- spark_stream.py
+++ spark_stream.py
@@ -1,39 +1,34 @@
-import os
-os.environ['PYSPARK_SUBMIT_ARGS'] = "--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.4.0 pyspark-shell"
-os.environ["PYSPARK_PYTHON"]="python3"
-os.environ["PYSPARK_DRIVER_PYTHON"]="python3"
-from pyspark import SparkContext
+# Execute as: ./spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.4.0 spark_stream.py /
+#             localhost:2181 twitter-stream  (Use zookeeper port, not Kafka one)
+
+import sys
+from pyspark import SparkContext, SparkConf
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
-import json
+from uuid import uuid1
 
-# Create Spark context
-def createContext():
+if __name__ == "__main__":
+
+    
     sc = SparkContext(appName="SparkStreaming")
-    sc.setLogLevel("WARN")
-    ssc = StreamingContext(sc, 5) # Create streaming context along with the batch duration in seconds
+    ssc = StreamingContext(sc, 10) # 10 second window
+    sc.setLogLevel("ERROR")
 
-    #Connect to Kafka as a consumer
-    #https://spark.apache.org/docs/latest/streaming-kafka-0-8-integration.html
-    kafkaStream = KafkaUtils.createStream(ssc, 'localhost:9092', 'spark-streaming', {'twitter-stream':1})
+    broker, topic = sys.argv[1:]
+    kafkaStream = KafkaUtils.createStream(ssc, broker, 'spark-streaming',{topic:1})
+    
+    #Extract tweets
+    lines = kafkaStream.map(lambda v: v[1])
+    createdAt_dstream = lines.map(lambda tweet: tweet['created_at'])
+    author_dstream = lines.map(lambda tweet: tweet['user']['screen_name'])
+    location_dstream = lines.map(lambda tweet: tweet['user']['location'])
 
-    # Extract tweets
-
-    parsed = kafkaStream.map(lambda v: json.loads(v[1]))
     count_this_batch = kafkaStream.count().map(lambda x:('Tweets in this batch: %s' % x)) #Count by batch
-    count_windowed = kafkaStream.countByWindow(60,5).map(lambda x:('Tweets total (One minute rolling count): %s' % x)) #Count by Window
-
-    #Get required fields for the tweets
-
-    createdAt_dstream = parsed.map(lambda tweet: tweet['created_at'])
-    author_dstream = parsed.map(lambda tweet: tweet['user']['screen_name'])
-    location_dstream = parsed.map(lambda tweet: tweet['user']['location'])
-
-    count_this_batch.union(count_windowed).pprint()
-
-    return ssc
-
-ssc = StreamingContext.getOrCreate('/tmp/checkpoint_v01',lambda: createContext())
-ssc.start()
-ssc.awaitTermination()
-
+    
+    count_this_batch.pprint()
+    createdAt_dstream.pprint()
+    author_dstream.pprint()
+    location_dstream.pprint()
+    
+    ssc.start()
+    ssc.awaitTermination()
